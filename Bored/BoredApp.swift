c0cf5a9a9d81beb7d9c2933742f26eacf2747701
Nihal Ismail <nihalismailk@gmail.com>
2023-11-08 18:30:15 +0530

    Initial Commit

---
+++ Bored/BoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredApp.swift
+//  Bored
+//
+//  Created by Nihal Ismail on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
