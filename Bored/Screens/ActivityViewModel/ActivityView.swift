4df440a37e0e35acc6cf8707b897bc5d960d6724
Nihal Ismail <nihalismailk@gmail.com>
2023-11-09 14:06:15 +0530

    refactor: code refactoring

--- Bored/Screens/ActivityViewModel/ActivityView.swift
+++ Bored/Screens/ActivityViewModel/ActivityView.swift
@@ -15,64 +15,76 @@ struct ActivityView: View {
         VStack{
             if viewModel.isLoading{
                 LoadingView()
-            } 
+            }
             else if viewModel.isError{
                 ErrorView(viewModel: viewModel)
             }
             else{
                 if let activity = viewModel.activity{
-                    VStack {
-                        Spacer()
-                        LottieView(name: activity.type)
-                            .scaleEffect(viewModel.animationSize)
-                            .frame(width: 300,height: 300)
-                        Text(activity.activity)
-                            .font(.largeTitle)
-                            .multilineTextAlignment(.center)
-                            .bold()
-                            .padding(.top,48)
-                        
-                        Spacer()
-                        Button(action: {
-                            
-                        }, label: {
-                            Link(destination: viewModel.activityLink, label: {
-                                Text("Get Started")
-                                    .font(.title2)
-                                    .bold()
-                                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0,
-                                           maxHeight: 60
-                                    )
-                                    .foregroundStyle(Color(uiColor: .label))
-                            })
-                        })
-                        .background(viewModel.activityThemeColor, in: .capsule)
-                        .padding(.bottom)
-                    }
-                    .padding()
-                    .overlay(alignment: .topTrailing, content: {
-                        Button(action: {
-                            viewModel.loadActivity()
-                        }, label: {
-                            Image(systemName: "arrow.clockwise")
-                                .padding()
-                                .imageScale(.large)
-                                .foregroundStyle(Color(uiColor: .label ))
-                                
-                        })
-                        .background(viewModel.activityThemeColor, in: .circle)
-                        .padding()
-                    })
+                    ActivityStack(activity: activity,viewModel: viewModel)
+                } else{
+                    ErrorView(viewModel: viewModel)
                 }
             }
         }
         .task {
             viewModel.loadActivity()
         }
-        
     }
 }
 
 #Preview {
     ActivityView()
+}
+
+struct ActivityStack: View {
+    let activity: Activity
+    let viewModel: ActivityViewModel
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            LottieView(name: activity.type)
+                .scaleEffect(viewModel.animationSize)
+                .frame(width: 300,height: 300)
+            Text(activity.activity)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .lineLimit(3)
+                .minimumScaleFactor(0.5)
+                .bold()
+                .padding(.top,48)
+            
+            Spacer()
+            Button(action: {
+                
+            }, label: {
+                Link(destination: viewModel.activityLink, label: {
+                    Text("Get Started")
+                        .font(.title2)
+                        .bold()
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0,
+                               maxHeight: 60
+                        )
+                        .foregroundStyle(Color(uiColor: .label))
+                })
+            })
+            .background(viewModel.activityThemeColor, in: .capsule)
+            .padding(.bottom)
+        }
+        .padding()
+        .overlay(alignment: .topTrailing, content: {
+            Button(action: {
+                viewModel.loadActivity()
+            }, label: {
+                Image(systemName: "arrow.clockwise")
+                    .padding()
+                    .imageScale(.large)
+                    .foregroundStyle(Color(uiColor: .label ))
+                
+            })
+            .background(viewModel.activityThemeColor, in: .circle)
+            .padding()
+        })
+    }
 }
